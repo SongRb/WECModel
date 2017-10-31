353330f674f56dbcfe1962634abe976f58d8006a
SongRb <tangsongkai2287@gmail.com>
2017-10-31 20:12:02 +0800

    Fix one crtical bug in ranking QA pair(dist.cosine is not cos(x))

--- WEC_rank.py
+++ WEC_rank.py
@@ -10,20 +10,20 @@ from parse_text import remove_punctuation
 
 class WECRanker:
     def __init__(self):
-        WORD2VEC_MODEL_PATH = os.path.join('data', 'yahoo-text.bin')
-        WEC_MODEL_PATH = os.path.join('data', 't_matrix.npy')
+        work_dir = os.path.join('work', 'L6-1')
+        WORD2VEC_MODEL_PATH = os.path.join(work_dir, 'YahooTextL6-1.bin')
+        WEC_MODEL_PATH = os.path.join(
+            work_dir, '20171031-141035-13200', 't_matrix.npy')
 
         self.w2v_model = word2vec.load(WORD2VEC_MODEL_PATH)
         self.t_matrix = np.load(WEC_MODEL_PATH)
 
-        with open(os.path.join('data', 'answer'), 'r') as ans_in, open(os.path.join('data', 'question'),
-                                                                       'r') as ques_in:
+        with open(os.path.join(work_dir, 'answer'), 'r') as ans_in, open(os.path.join(work_dir, 'question'),
+                                                                         'r') as ques_in:
             self.answer_list = ans_in.read().split('\n')
             self.question_list = ques_in.read().split('\n')
             self.answer_list = [unicode(i) for i in self.answer_list]
             self.question_list = [unicode(i) for i in self.question_list]
-
-        pass
 
     @staticmethod
     def process_text(s):
@@ -39,14 +39,16 @@ class WECRanker:
         for i in answer_words:
             max_score = 0
             if i in self.w2v_model:
+                eli_ans_vec = np.matmul(self.t_matrix, self.w2v_model[i])
                 for j in question_words:
                     if j in self.w2v_model:
-                        score = dis.cosine(self.w2v_model[j], np.matmul(self.w2v_model[i], self.t_matrix))
+                        score = 1 - dis.cosine(self.w2v_model[j], eli_ans_vec)
                         if score > max_score:
                             max_score = score
-            total_score.append(max_score)
-
-        return sum(total_score) / len(total_score)
+                total_score.append(max_score)
+        if len(total_score) != 0:
+            return sum(total_score) / len(total_score)
+        return 0
 
     def cal_set(self, answer_list, question):
         return [self.cal_score(answer, question) for answer in answer_list]
@@ -54,14 +56,15 @@ class WECRanker:
     def validate_model(self):
         count = 0
         for index in range(len(self.question_list)):
-            if index%200 == 0:
-                print 'Rate:',float(count)/(index+1),
+            if index % 50 == 0:
+                print 'Rate:', float(count) / (index + 1),
                 print "{0} processed".format(index)
             qa_pair = (self.question_list[index], self.answer_list[index])
-            answer_list = random.sample(self.question_list, 50)
+            answer_list = random.sample(self.question_list, 6)
             score_list = self.cal_set(answer_list, qa_pair[0])
             score = self.cal_score(qa_pair[1], qa_pair[0])
-            if score >= sorted(score_list)[-2]:
+            score_list = sorted(score_list)
+            if score > score_list[-1]:
                 count += 1
 
         return float(count) / len(self.question_list)
--- WEC_train.py
+++ WEC_train.py
@@ -7,15 +7,20 @@ import time
 import numpy as np
 import tensorflow as tf
 
+train_size = 0
+
 
 def cos_func(a, b):
     normalize_a = tf.nn.l2_normalize(a, 0)
     normalize_b = tf.nn.l2_normalize(b, 0)
-    return -tf.reduce_sum(tf.multiply(normalize_a, normalize_b))
+
+    return tf.to_float(tf.shape(normalize_a)[1], name='ToFloat') - tf.reduce_sum(tf.multiply(normalize_a, normalize_b))
+
 
 def trans(a):
     a = np.asarray(a)
     return a.transpose()
+
 
 def load_model(filename):
     with tf.Session() as sess:
@@ -24,18 +29,20 @@ def load_model(filename):
         tf.initialize_all_variables().run()
         return sess.run('t_matrix:0')
 
+
 def random_init():
     return np.random.uniform(low=-0.01, high=0.01, size=(DIMENSIONS, DIMENSIONS))
 
+
 def saved_init(filename):
     return np.load(filename)
-    
 
 work_dir = os.path.join('work', 'L6-1')
 dataset_name = 'WEC-L6-1-input.npz'
+model_id = '20171031-141035-13200'
 
 
-npzfile = np.load(os.path.join(work_dir,'WEC-L6-1-input.npz'))
+npzfile = np.load(os.path.join(work_dir, dataset_name))
 train_x = npzfile['x']
 train_y = npzfile['y']
 train_w = npzfile['w']
@@ -52,14 +59,14 @@ np.random.shuffle(ds)
 TRAIN_RATIO = 0.6  # 60% of the dataset is used for training
 _train_size = int(DS_SIZE * TRAIN_RATIO)
 _test_size = DS_SIZE - _train_size
-ALPHA = 3e-7  # learning rate
+ALPHA = 1e-5  # learning rate
 LAMBDA = 0.5  # L2 regularization factor
 TRAINING_STEPS = 20001
 # Swap train data and test data
-train_data, train_labels = map(trans,zip(*ds[_train_size:]))
-
-test_data, test_labels =map(trans, zip(*ds[0:_train_size]))
-
+train_data, train_labels = map(trans, zip(*ds[0:_train_size]))
+train_size = len(train_data[0])
+test_data, test_labels = map(trans, zip(*ds[_train_size:]))
+test_size = len(test_data[0])
 print('Dataset generated')
 
 # define the computational graph
@@ -67,30 +74,21 @@ print('Setting preferences')
 graph = tf.Graph()
 with graph.as_default():
     # declare graph inputs
-    # x_train = tf.placeholder(tf.float32, shape=(_train_size, DIMENSIONS))
-    # y_train = tf.placeholder(tf.float32, shape=(_train_size, DIMENSIONS))
-    # x_test = tf.placeholder(tf.float32, shape=(_test_size, DIMENSIONS))
-    # y_test = tf.placeholder(tf.float32, shape=(_test_size, DIMENSIONS))
-
-    # Now we swap test set and train set
-    # 
-    x_train = tf.placeholder(tf.float32, shape=( DIMENSIONS,_test_size,))
-    y_train = tf.placeholder(tf.float32, shape=(DIMENSIONS,_test_size))
-    x_test = tf.placeholder(tf.float32, shape=( DIMENSIONS,_train_size,))
-    y_test = tf.placeholder(tf.float32, shape=(DIMENSIONS,_train_size))
+    x_train = tf.placeholder(tf.float32, shape=(DIMENSIONS, train_size))
+    y_train = tf.placeholder(tf.float32, shape=(DIMENSIONS, train_size))
+    x_test = tf.placeholder(tf.float32, shape=(DIMENSIONS, test_size))
+    y_test = tf.placeholder(tf.float32, shape=(DIMENSIONS, test_size))
+    
+    # theta = tf.Variable(
+    # np.random.uniform(low=-0.1, high=0.1, size=(DIMENSIONS, DIMENSIONS)),
+    # dtype=np.float32, name='t_matrix')
 
     theta = tf.Variable(
-        np.random.uniform(low=-0.1, high=0.1, size=(DIMENSIONS, DIMENSIONS)),
+        saved_init(os.path.join(
+            work_dir, model_id, 't_matrix.npy')),
         dtype=np.float32, name='t_matrix')
 
-    # theta = tf.Variable(
-    #     saved_init(os.path.join('work','L6-1','20171029-202021-800','t_matrix.npy')),
-    #     dtype=np.float32, name='t_matrix')
-
-
     # forward propagation
-    # train_prediction = tf.matmul(x_train, theta)
-    # test_prediction = tf.matmul(x_test, theta)
     train_prediction = tf.matmul(theta, x_train)
     test_prediction = tf.matmul(theta, x_test)
     train_cost = cos_func(train_prediction, y_train)
@@ -102,7 +100,7 @@ print('Running...')
 saver = tf.train.Saver({'t_matrix': theta})
 with tf.Session(graph=graph) as s:
     tf.initialize_all_variables().run()
-    print("initialized")
+    print("Initialized")
     print(theta.eval())
     for step in range(TRAINING_STEPS):
         _, train_c, test_c = s.run([optimizer, train_cost, test_cost],
@@ -111,17 +109,15 @@ with tf.Session(graph=graph) as s:
                                               x_test: test_data,
                                               y_test: test_labels})
         if step % 200 == 0:
-            # it should return bias close to zero and parameters all close to 1 (see definition of f)
             print("\nAfter", step, "iterations:")
-            # print("   Bias =", theta_0.eval(), ", Weights = ", theta.eval())
-            print("   train cost =", train_c)
-            print("   test cost =", test_c)
-            time_str = time.strftime("%Y%m%d-%H%M%S")
-            time_str+='-'
-            time_str+=str(step)
+            print("  Relative train cost =", train_c / train_size)
+            print("  Relative test cost =", test_c / test_size)
+            time_str = time.strftime("%Y%m%d-%H%M%S")+'-'+str(step)
             os.makedirs(os.path.join(work_dir, time_str))
-            save_path = saver.save(s, os.path.join(work_dir, time_str, 'model.ckpt'))
-            with open(os.path.join(work_dir, time_str,'status.txt'),'w') as fout:
-                fout.write(str(train_c)+' '+str(test_c))
-            np.save(os.path.join(work_dir, time_str,'t_matrix'),s.run('t_matrix:0'))
-            print("Saved in",save_path)
+            save_path = saver.save(s, os.path.join(
+                work_dir, time_str, 'model.ckpt'))
+            with open(os.path.join(work_dir, time_str, 'status.txt'), 'w') as fout:
+                fout.write('{0} {1}'.format(str(train_c),str(test_c)))
+            np.save(os.path.join(work_dir, time_str,
+                                 't_matrix'), s.run('t_matrix:0'))
+            print("Saved in", save_path)
